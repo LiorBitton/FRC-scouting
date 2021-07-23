e3673617b7a95f15a7313bf64aee2cdea7dc45a0
roil <liorb5000@gmail.com>
2021-07-23 13:38:24 +0300

    Update scout_manager.dart
    Users cant exit from ScoutManager using back button, and changed tab indicator color

--- scouting_application/lib/screens/scouting/scout_manager.dart
+++ scouting_application/lib/screens/scouting/scout_manager.dart
@@ -16,24 +16,31 @@ class ScoutManager extends StatefulWidget {
 class _ScoutManagerState extends State<ScoutManager> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-          appBar: AppBar(
-            automaticallyImplyLeading: false,
-            bottom: TabBar(
-              tabs: [
-                Tab(text: "אוטונומי", icon: Icon(Icons.do_not_touch)),
-                Tab(text: "Teleop", icon: Icon(Icons.drive_eta_rounded)),
-                Tab(text: "Endgame", icon: Icon(Icons.elevator)),
-              ],
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: DefaultTabController(
+        length: 3,
+
+        child: Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              bottom: TabBar(
+                indicatorColor: Color.fromARGB(255, 50, 50, 35),
+                indicatorSize: TabBarIndicatorSize.tab,
+                indicatorWeight: 5,
+                tabs: [
+                  Tab(text: "אוטונומי", icon: Icon(Icons.do_not_touch)),
+                  Tab(text: "Teleop", icon: Icon(Icons.drive_eta_rounded)),
+                  Tab(text: "Endgame", icon: Icon(Icons.elevator)),
+                ],
+              ),
             ),
-          ),
-          body: TabBarView(children: [
-            widget.autonomous,
-            widget.teleoperated,
-            widget.endgame
-          ])),
+            body: TabBarView(children: [
+              widget.autonomous,
+              widget.teleoperated,
+              widget.endgame
+            ])),
+      ),
     );
   }
 }
