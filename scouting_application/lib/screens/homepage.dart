b37c4be4301f4d0e06d26c11c90fb83f0c3c86b3
LiorBitton <liorb5000@gmail.com>
2022-05-19 13:46:39 +0300

    Homepage Redesign

--- scouting_application/lib/screens/homepage.dart
+++ scouting_application/lib/screens/homepage.dart
@@ -2,18 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:scouting_application/classes/database.dart';
 import 'package:scouting_application/classes/global.dart';
 import 'package:scouting_application/screens/admin/admin_settings.dart';
-import 'package:scouting_application/screens/stats/stats_lobby.dart';
 import 'package:scouting_application/screens/scouting/scouting_menu.dart';
 import 'package:scouting_application/screens/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:scouting_application/screens/stats/stats_lobby.dart';
+import 'package:scouting_application/themes/custom_themes.dart';
 
 class Homepage extends StatelessWidget {
   Homepage({Key? key}) : super(key: key);
   final FirebaseAuth auth = FirebaseAuth.instance;
-
   @override
   Widget build(BuildContext context) {
     onStart(context);
+    var brightness = MediaQuery.of(context).platformBrightness;
+    bool isDarkMode = brightness == Brightness.dark;
     return WillPopScope(
       onWillPop: () async => false,
       child: Scaffold(
@@ -53,24 +55,59 @@ class Homepage extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                IconButton(
-                    icon: Icon(Icons.touch_app),
-                    iconSize: 50,
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ScoutingMenu()));
-                    }),
-                IconButton(
-                    icon: Icon(Icons.query_stats),
-                    iconSize: 50,
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => StatsLobby()));
-                    }),
+                Container(
+                  decoration: ShapeDecoration(
+                      color: Colors.white,
+                      shape: CircleBorder(),
+                      shadows: [
+                        BoxShadow(
+                          color: Colors.white,
+                          spreadRadius: 8,
+                          blurRadius: 0,
+                        )
+                      ]),
+                  padding: EdgeInsets.all(8),
+                  child: IconButton(
+                      icon: Icon(Icons.touch_app_rounded),
+                      iconSize: 60,
+                      color: isDarkMode
+                          ? CustomTheme.darkTheme.scaffoldBackgroundColor
+                          : CustomTheme.lightTheme.scaffoldBackgroundColor,
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ScoutingMenu()));
+                      }),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  decoration: ShapeDecoration(
+                      color: isDarkMode
+                          ? CustomTheme.darkTheme.scaffoldBackgroundColor
+                          : CustomTheme.lightTheme.scaffoldBackgroundColor,
+                      shape: CircleBorder(),
+                      shadows: [
+                        BoxShadow(
+                          color: Colors.white,
+                          spreadRadius: 8,
+                          blurRadius: 0,
+                        )
+                      ]),
+                  padding: EdgeInsets.all(8),
+                  child: IconButton(
+                      color: Colors.white,
+                      icon: Icon(Icons.query_stats_rounded),
+                      iconSize: 60,
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => StatsLobby()));
+                      }),
+                ),
               ],
             ),
           )),
