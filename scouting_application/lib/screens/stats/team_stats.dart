a6236fa9bfcbcaf38f5077dac87074f541612cd4
Lior Bitton <liorb5000@gmail.com>
2023-02-27 22:29:33 +0200

    generic stats page first test

--- scouting_application/lib/screens/stats/team_stats.dart
+++ scouting_application/lib/screens/stats/team_stats.dart
@@ -5,9 +5,24 @@ class TeamStats extends StatelessWidget {
   final String teamID;
   @override
   Widget build(BuildContext context) {
+    final FirebaseDatabase db = FirebaseDatabase.instance;
+    db.ref("teams/$teamID/${Global.currentEventKey}/custom").get;
+    DataSnapshot snapshot = await db
+        .ref("teams/$teamID/${Global.currentEventKey}/custom")
+        .get()
+        .timeout(Duration(seconds: 5));
+    if (!snapshot.exists) {
+      return {};
+    }
+    Map<String, dynamic> data = Map<String, dynamic>.from(
+        snapshot.value as Map<dynamic, dynamic>);
+    List<Map<String,String>> keys = [];
+    for(MapEntry<String, dynamic> entry in data.entries){
+      keys.append({entry.key:entry.value.toString()});
+    }
     return Scaffold(
       appBar: AppBar(title: Text("Team $teamID - Stats")),
-      // body: CategoryList(data: ["okay", "cool", "wow🎈"], title: "stats"),
+      body: CategoryList(data: keys, title: "stats"),
     );
   }
 }
